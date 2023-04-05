91add0b4617efb21d9399e1ab66987467a8fcf66
Elvin Ezizli <elvin@example.com>
2023-04-06 02:06:38 +0400

    .

---
+++ 5AprelTaskHome/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5AprelTaskHome
+{
+    internal class Product
+    {
+        public readonly string CreatDate;
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private int _price;
+        public int Price
+
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+       public Product(int price,string name,string description, string creatdate)
+        {
+            this.Price = price;
+            this.Name = name;   
+            this.Description = description; 
+            this.CreatDate = creatdate; 
+        }
+
+        protected internal void GetDetails()
+        {
+            Console.WriteLine($"{Price} {Name} {Description} {CreatDate}");
+
+        }
+    }
+}
---
+++ 5AprelTaskHome/Program.cs
@@ -0,0 +1,16 @@
+﻿using _5AprelTaskHome;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Product product=new Product(12,"BMW","Qara",DateTime.Now.ToString());
+        
+
+        product.GetDetails();
+
+
+        
+        
+    }
+}
